e3722d3f45ef02e8485d248c23134cdd5ad5a0c5
bazz666 <herrera.se1992@gmail.com>
2021-08-31 15:42:27 -0400

    done

--- pokedex.js
+++ pokedex.js
@@ -7,8 +7,7 @@ $(document).ready(function(){
         fetchPokemon();
     })
 
-
-
+    fetchPokemon();
 
     function fetchPokemon() {
         $.ajax({
@@ -19,10 +18,10 @@ $(document).ready(function(){
                 console.log(endpoint);
                 response.results.forEach(function (pokemon) {
                     let list = `<div class='card'>
-                    <div class='card-body'> 
+                    <div class='card-body'>
                     <h5 class='card-title'>${pokemon.name}</h5>
                     <a href='#' url='${pokemon.url}' 
-                    class='btn btn-primary'>Quiero Saber Mas!!!</a> 
+                    class='btn btn-primary'>quiero saber mas...</a> 
                     </div>
                     </div>`
                     $(".pokeCard").append(list)
@@ -40,27 +39,28 @@ $(document).ready(function(){
                             url: data,
                             method: 'GET',
                             success: function (response) {
-                                let name = response.name
-                                let type =''
+                                let name = response.name 
+                                let typ ='Types:'
                                 response.types.forEach(function(types){
                                     typ = typ + ' ' + types.type.name
                                 })
-                                let gen = response.sprites.front_shiny
-                                let abi = ''
+                                let image = response.sprites.front_shiny
+
+                                let abi = 'Abilities:'
                                 response.abilities.forEach(function (abilities) {
                                     abi = abi + ' ' + abilities.ability.name
                                 })
-                                let mov = ''
+                                let mov = 'Move set:'
                                 response.moves.forEach(function (moves, index) {
                                     if (index < 5) {
                                         mov = mov + ' ' + moves.move.name
                                     }
                                 })
                                 $('#exampleModalLabel').html(name)
-                                $('#type').html(typ)
-                                $('#abilities').html(abi)
-                                $('#moves').html(mov)
-                                $('#gen').html(` <img src="${gen}" alt="">`)
+                                $('#pokemonTypes').html(typ)
+                                $('#pokemonAbs').html(abi)
+                                $('#pokemonMoves').html(mov)
+                                $('#pokemonImage').html(` <img src="${image}" alt="">`)
 
                             }
                         })
